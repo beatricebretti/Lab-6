c6f57063754b6067633b70da23f8ced1cc471282
beatricebretti <bavaldes1@miuandes.cl>
2024-10-12 23:24:03 -0300

    Add Authorization

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :post
-    belongs_to :user
+    belongs_to :user, optional: true
     
     validates :content, :author, presence: true
     validates_with AuthorEmailValidator
